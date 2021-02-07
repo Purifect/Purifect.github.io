52c09b76d4b419c7cc1290ba63bba05e8e6f83d0
Puri Rudick <puri@Puris-MBP-2020.attlocal.net>
2021-02-06 22:26:11 -0600

    Add Shiny Link

---
+++ _site/BeerData/server.R
@@ -0,0 +1,100 @@
+server <- function(input, output, session) {
+    
+    output$beers <- renderTable({
+        req(input$beers)
+        beers <- read.csv(input$beers$datapath)
+        head(beers)
+    })
+    
+    output$breweries <- renderTable({
+        req(input$breweries)
+        breweries <- read.csv(input$breweries$datapath)
+        head(breweries)
+    })
+    
+    output$ABVhist <- renderPlot({
+        beers <- read.csv(input$beers$datapath)
+        breweries <- read.csv(input$breweries$datapath)
+        mergedata <- merge(x = beers, y = breweries,
+                           by.x = "Brewery_id", by.y = "Brew_ID", all = TRUE)
+        
+        mergedata <- mergedata %>% filter(State %in% input$id)
+        
+        if(input$dist == "hist")
+        {   
+            hist(mergedata$ABV,
+                 probability = TRUE,
+                 xlab = "ABV",
+                 main = "Alcohol by Volume (ABV)")
+        }
+        else
+        {
+            boxplot(mergedata$ABV,
+                    main = "Alcohol by Volume (ABV)",
+                    horizontal = TRUE)
+        }
+    })
+    
+    output$IBUhist <- renderPlot({
+        beers <- read.csv(input$beers$datapath)
+        breweries <- read.csv(input$breweries$datapath)
+        mergedata <- merge(x = beers, y = breweries,
+                           by.x = "Brewery_id", by.y = "Brew_ID", all = TRUE)
+        
+        mergedata <- mergedata %>% filter(State %in% input$id) %>% filter(!is.na(IBU))
+        mergedata <- as.vector(mergedata$IBU)
+        
+        if(input$dist == "hist")
+        {
+            hist(mergedata,
+                 probability = TRUE,
+                 xlab = "IBU",
+                 main = "International Bitterness Units (IBU)")
+        }
+        else
+        {
+            boxplot(mergedata,
+                    main = "International Bitterness Units (IBU)",
+                    horizontal = TRUE)
+        }
+    })
+    
+    observe({
+        req(input$breweries)
+        breweries <- read.csv(input$breweries$datapath)
+        breweries <- breweries[order(breweries$State),]
+        state <- unique(breweries$State)
+        
+        updatePickerInput(
+            session,
+            "id",
+            choices = unique(breweries$State),
+            select = state
+        )
+    })
+    
+    output$scatter <- renderPlot({
+        beers <- read.csv(input$beers$datapath)
+        breweries <- read.csv(input$breweries$datapath)
+        mergedata <- merge(x = beers, y = breweries,
+                           by.x = "Brewery_id", by.y = "Brew_ID", all = TRUE)
+        
+        mergedata <- mergedata %>% filter(State %in% input$id)
+        
+        if(input$line == 0)
+        {
+            ggplot(data=mergedata,
+                   aes(x=ABV, y=IBU),
+                   main = "ABV vs. IBU Scatter Plot") + 
+                geom_point()
+        }
+        else
+        {
+            ggplot(data=mergedata,
+                   aes(x=ABV, y=IBU),
+                   main = "ABV vs. IBU Scatter Plot") +
+                geom_point() + 
+                geom_smooth(method = 'lm')
+        }
+    })
+}
---
+++ _site/BeerData/ui.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(shinyWidgets)
+library(dplyr)
+library(ggplot2)
+
+# Define UI for random distribution app ----
+ui <- fluidPage(
+    
+    # App title ----
+    titlePanel("Beers and Breweries in the USA"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            fileInput("beers", "Upload Beers Data", accept = ".csv"),
+            fileInput("breweries", "Upload Breweries Data", accept = ".csv"),
+            br(),
+            
+            # Input: Select the random distribution type ----
+            radioButtons("dist", "Distribution Plot:",
+                         c("Histogram" = "hist",
+                           "Box Plot" = "box")),
+            br(),
+            
+            pickerInput(
+                inputId = "id", label = "State(s) :",
+                "",
+                options = list(`actions-box` = TRUE, `selected-text-format` = "count > 2",
+                               `count-selected-text` = "{0}/{1} States"),
+                multiple = TRUE
+            ),
+            br(),
+            
+            checkboxInput("line",
+                               label = "Add Linear Regression Line")
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Tabset w/ plot, summary, and table ----
+            tabsetPanel(type = "tabs",
+                        tabPanel("Data Table", 
+                                 h4("Beers Data"),
+                                 tableOutput("beers"), 
+                                 br(),
+                                 h4("Breweries Data"),
+                                 tableOutput("breweries")),
+                        tabPanel("ABV and IBU Distribution", 
+                                 plotOutput("ABVhist"),
+                                 plotOutput("IBUhist")),
+                        tabPanel("ABV vs. IBU Scatter Plot", 
+                                 plotOutput("scatter"))
+                        
+            )
+        )
+    )
+)
